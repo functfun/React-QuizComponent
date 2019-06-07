fc178ae9e49d9daf4cdd583465f95aae6359627f
functfun <51472386+functfun@users.noreply.github.com>
2019-06-07 13:51:34 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,9 +10,13 @@ class Quiz extends Component{
   render()
   {
     return
+    (
     <div>
-      <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+      <div className="QuizQuestion">
+       {quizData.quiz_questions[0].instruction_text}
+      </div>
     </div>
+  )
   }
 }
 
