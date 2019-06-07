3bfe9442a10edbce15c83d7f84efddcdf5535111
functfun <51472386+functfun@users.noreply.github.com>
2019-06-07 15:11:32 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,9 +5,9 @@ let quizData = require('./quiz_data.json');
 
 class Quiz extends Component{
   constructor(props){
-    super(props)
-    this.state = {quiz_position:1}
-   }
+    super(props);
+    this.state = {quiz_position:1} };
+
 
   render()
   {
