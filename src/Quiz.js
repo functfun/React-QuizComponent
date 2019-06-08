99f253de0dc443c38876ed0162bfeaa934b607e4
functfun <51472386+functfun@users.noreply.github.com>
2019-06-07 21:35:13 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,27 +1,18 @@
-
-
 import React, { Component } from 'react'
-
 import QuizQuestion from './QuizQuestion.js'
-
 import QuizEnd from './QuizEnd.js'
-
-
 
 let quizData = require('./quiz_data.json')
 
-
-
 class Quiz extends Component {
-
 constructor(props) {
-
  super(props)
-
  this.state = { quiz_position: 1 }
-
 }
 
+showNextQuestion(){
+
+}
 render() {
 
  const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
@@ -30,7 +21,7 @@ render() {
 
    <div>
 
-     {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+     {isQuizEnd ? <QuizEnd /> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
 
    </div>
 
