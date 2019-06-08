5a5dfecc4030d0a7232e13c554e77e9aef3b855e
functfun <51472386+functfun@users.noreply.github.com>
2019-06-07 22:02:20 -0400

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,7 +4,7 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component{
   constructor(props){
     super(props)
-    this.setState={incorrectAnswer:false}
+    this.state={incorrectAnswer:false}
   }
   handleClick(buttonText){
     if (buttonText===this.props.quiz_question.answer){
