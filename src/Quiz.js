e25f48ae85af8e626d8a26c7f4035ead66950679
functfun <51472386+functfun@users.noreply.github.com>
2019-06-07 20:49:12 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,26 +1,45 @@
-import React, {Component} from 'react'
+
+
+import React, { Component } from 'react'
+
 import QuizQuestion from './QuizQuestion.js'
+
 import QuizEnd from './QuizEnd.js'
 
-let quizData = require('./quiz_data.json');
-
-class Quiz extends Component{
-  constructor(props){
-    super(props);
-    this.state = {quiz_position:1}
-  //  isQuizEnd:false
-  }
 
 
-  render()
-  {
-    const isQuizEnd =true
-    return(
-      <div>
-         {isQuizEnd ? <QuizEnd/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/> }
-      </div>
-  )
-  }
+let quizData = require('./quiz_data.json')
+
+
+
+class Quiz extends Component {
+
+constructor(props) {
+
+ super(props)
+
+ this.state = { quiz_position: 1 }
+
 }
+
+render() {
+
+ const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+
+ return (
+
+   <div>
+
+     {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+
+   </div>
+
+ )
+
+}
+
+}
+
+
 
 export default Quiz
