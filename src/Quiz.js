34c433daad7129bd2709444409c41617d7513841
functfun <51472386+functfun@users.noreply.github.com>
2019-06-07 20:30:09 -0400

    Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,27 +1,48 @@
-import React, {Component} from 'react'
-import QuizQuestion from './QuizQuestion.js'
-import QuizEnd from './QuizEnd.js'
+import React, {Component} from 'react';
+import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
-class Quiz extends Component{
-  constructor(props){
-    super(props);
-    this.state = {quiz_position:1
-  //  isQuizEnd:false
-  };
-  }
+class Quiz extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quiz_position: 1,
+            isQuizEnd: false
+        };
+    }
 
+    showNextQuestion() {
+        if(this.state.quiz_position === quizData.quiz_questions.length) {
+            this.setState({
+                isQuizEnd: true
+            })
+        }
+        else {
+            this.setState(prevState => ({
+                quiz_position: prevState.quiz_position + 1
+            }));
+        }
+    }
 
-  render()
-  {
-    const isQuizEnd =false
-    return(
-      <div>
-         {isQuizEnd ? <QuizEnd/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/> }
-      </div>
-  )
-  }
+    handleResetClick() {
+        this.setState({
+            quiz_position: 1
+        });
+    }
+
+    render() {
+        return <div>
+            <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+            {
+                this.state.isQuizEnd ?
+                <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
+                ''
+            }
+
+        </div>;
+    }
 }
 
-export default Quiz
+export default Quiz;
